9ed35c939251949fdbf4d96265e1c995feb75fd5
taemin <taemin555@naver.com>
2022-11-24 11:10:43 +0900

    detect.py 대문자, 숫자 처리 및 인식 결과 string화
    number, upper boolean 변수를 이용한 간단한 convert과정 추가
    리스트 정렬을 이용한 x좌표 기준 정렬 및 루프를 통한 string화

--- detect.py
+++ detect.py
@@ -14,105 +14,53 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
-######Flask 서버를 위한 코드########
-from flask import Flask
-import werkzeug
-from flask_ngrok2 import run_with_ngrok
-from flask import request, Response, jsonify, render_template
-import os
+random.seed(1)
 
-app = Flask(__name__)
-run_with_ngrok(app=app, auth_token='2HgnZng3yM5ovJYPZ8c6aE0SgBX_3YYboKmpBGT5EeCAx3fU2')   
+def convert(cls, number, upper):
+    if not upper and not number:
+        return chr(cls+97)
+    elif upper:
+        return chr(cls+65)
+    else:
+        if 0 <= cls < 9:
+            return chr(cls+49)
+        elif cls == 10:
+            return '0'
+        else:
+            return 'Error'
 
-@app.route("/")
-def home():
-    return "<h1>GFG is great platform to learn</h1>"
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
 
-@app.route('/uploader', methods=['GET', 'POST'])
-def handle_request():
-    imagefile = request.files['image']
-    filename = werkzeug.utils.secure_filename(imagefile.filename)
-    print("\nReceived image File name : " + imagefile.filename)
-    savepath=os.path.join(os.getcwd(), 'get-images', filename)
-    imagefile.save(savepath)
-    print('savepath : ' + savepath)
-    global source
-    source = savepath
-    detect(source)# yolov7으로 ls한후에 사용해야 할 듯!
-    return "Image Uploaded Successfully"
-####################################
+    # Directories
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
-parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-parser.add_argument('--view-img', action='store_true', help='display results')
-parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-parser.add_argument('--augment', action='store_true', help='augmented inference')
-parser.add_argument('--update', action='store_true', help='update all models')
-parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-parser.add_argument('--name', default='exp', help='save results to project/name')
-parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
-# weights, conf_thre, --img-size --source 옵션만 사용했음
-opt = parser.parse_args()
-print(opt)
-#check_requirements(exclude=('pycocotools', 'thop'))
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
 
-source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
-save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
-webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-    ('rtsp://', 'rtmp://', 'http://', 'https://'))
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
 
-# Directories
-save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-(save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
 
-# Initialize
-set_logging()
-device = select_device(opt.device)
-half = device.type != 'cpu'  # half precision only supported on CUDA
+    if half:
+        model.half()  # to FP16
 
-# Load model
-model = attempt_load(weights, map_location=device)  # load FP32 model
-stride = int(model.stride.max())  # model stride
-imgsz = check_img_size(imgsz, s=stride)  # check img_size
-if trace:
-    model = TracedModel(model, device, opt.img_size)
-if half:
-    model.half()  # to FP16
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
 
-# Second-stage classifier
-classify = False
-if classify:
-    modelc = load_classifier(name='resnet101', n=2)  # initialize
-    modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
-# 여기에 서버에서 얻은 이미지 넣는 로직 넣으면 될듯?
-
-def detect(soruce):
-    print(source)
-    global weights
-    print(weights)
-    global view_img
-    global save_txt
-    global imgsz
-    global trace
-    global save_img
-    global webcam
-    global save_dir
-    global device
-    global half 
-    global model
-    global stride
-    global classify
-    view_img=False
     # Set Dataloader
     vid_path, vid_writer = None, None
     if webcam:
@@ -133,6 +81,7 @@ def detect(soruce):
     old_img_b = 1
 
     t0 = time.time()
+    startTime = 0
     for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -150,8 +99,7 @@ def detect(soruce):
 
         # Inference
         t1 = time_synchronized()
-        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=opt.augment)[0]
+        pred = model(img, augment=opt.augment)[0]
         t2 = time_synchronized()
 
         # Apply NMS
@@ -173,6 +121,8 @@ def detect(soruce):
             save_path = str(save_dir / p.name)  # img.jpg
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            
+            classes = []
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
@@ -193,20 +143,51 @@ def detect(soruce):
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                    
+                    # 각 개체별 class와 왼쪽 x 좌표를 insert    
+                    classes.append([cls, xyxy[0]])
+                 
+            # 리스트의 각 칸이 [class, x좌표]의 형태일 때 x좌표를 기준으로 정렬   
+            sorted_cls = sorted(classes, key=lambda x : x[1])
+            
+            word = ''
+            number, upper = False, False
+            
+            for braille in sorted_cls:
+                cls = braille[0]
+                if cls == 26:
+                    number = True
+                elif cls == 27:
+                    upper = True
+                else:
+                    word += convert(cls, number, upper)
+                    number, upper = False, False
+            print(word)
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
 
             # Stream results
+            # @prams p : 파일 주소
+            # @prams s : 검출된 물체 이름
+            if dataset.mode != 'image':
+                currentTime = time.time()
+
+                fps = 1/(currentTime - startTime)
+                startTime = currentTime
+
+                cv2.putText(im0, "FPS: " + str(int(fps)), (20, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0),2)
             if view_img:
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
             # Save results (image with detections)
+            # 파일로 저장
             if save_img:
+                # 이미지인 경우
                 if dataset.mode == 'image':
                     cv2.imwrite(save_path, im0)
-                    print(f" The image with the result is saved in: {save_path}")
+                # print(f" The image with the result is saved in: {save_path}")
                 else:  # 'video' or 'stream'
                     if vid_path != save_path:  # new video
                         vid_path = save_path
@@ -223,6 +204,7 @@ def detect(soruce):
                     vid_writer.write(im0)
 
     if save_txt or save_img:
+        print(f" The output with the result is saved in: {save_path}")
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         #print(f"Results saved to {save_dir}{s}")
 
@@ -230,13 +212,33 @@ def detect(soruce):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+
     with torch.no_grad():
         if opt.update:  # update all models (to fix SourceChangeWarning)
             for opt.weights in ['yolov7.pt']:
                 detect()
                 strip_optimizer(opt.weights)
         else:
-            app.run()
-
-
-
+            detect()
